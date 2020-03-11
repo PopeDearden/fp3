9a2a84fec069d9ca61d8140751cdbfc19c9c8a79
taylor dearden <taylordearden@gmail.com>
2020-03-11 17:55:51 -0600

    Director in progress table works with filters and totals

--- src/components/Director/OrdersInProgress.jsx
+++ src/components/Director/OrdersInProgress.jsx
@@ -65,23 +65,46 @@ class OrdersInProgress extends Component {
       possible: possible.toFixed(2),
     })
   }
+  calculate = (orders, student, type) => {
+  
+    let filtered = orders.filter((element) => { return element.user_id === student.user_id })
+    let totalFlashBlack = 0
+    let totalFlashYellow = 0
+    let totalLanternYellow = 0 
+    let totalLanternBlack = 0
+    for (let i = 0; i < filtered.length; i++) {
+      totalFlashBlack += filtered[i].flashlights
+    }
+    for (let i = 0; i < filtered.length; i++) {
+      totalFlashYellow += filtered[i].flashlight_yellow
+    }
+    for (let i = 0; i < filtered.length; i++) {
+      totalLanternYellow += filtered[i].pucs
+    }
+    for (let i = 0; i < filtered.length; i++) {
+      totalLanternBlack += filtered[i].puc_black
+    }
+    let totalPrice = totalFlashBlack *30 + totalFlashYellow *30 + totalLanternYellow *35 +totalLanternBlack *35
+    return {totalFlashBlack, totalFlashYellow, totalLanternYellow, totalLanternBlack, totalPrice }
+  }
+  
 
   buildTable = (orders, id) => {
-   let filtered = orders.filter((element) => {
-     return element.user_id === id
-   })
-   return filtered
+    let filtered = orders.filter((element) => {
+      return element.user_id === id
+    })
+    return filtered
 
   }
 
   render() {
-    const orders2 = this.state.orders.filter((element, index) => {
+    const student2 = this.state.students.filter((element, index) => {
       return element.last_name.toLowerCase().includes(this.state.searchInput2.toLowerCase())
     })
-    const orders1 = orders2.filter((element, index) => {
+    const student = student2.filter((element, index) => {
       return element.first_name.toLowerCase().includes(this.state.searchInput3.toLowerCase())
     })
-    const orders = orders1.filter((element, index) => {
+    const orders = this.state.orders.filter((element, index) => {
       return element.first_name_cust.toLowerCase().includes(this.state.searchInput.toLowerCase()) || element.last_name_cust.toLowerCase().includes(this.state.searchInput.toLowerCase())
     })
     return (
@@ -141,52 +164,55 @@ class OrdersInProgress extends Component {
         <input class="Search" placeholder={"Search by student last initial"} onChange={e => this.setState({ searchInput2: e.target.value })} />    <i class="fas fa-search"></i>
         <input class="Search" placeholder={"Search by customer name"} onChange={e => this.setState({ searchInput: e.target.value })} />    <i class="fas fa-search"></i>
         <div class="TableContainer">
-          <table class="InProgressTable">
-            <tr id="TableHeader">
-              <th id="restrictTable">Student<br></br>Name</th>
-              <th id="restrictTable">Customer<br></br></th>
-              <th id="restrictTable">Contact</th>
-              <th id="restrictTable">Date Ordered</th>
-              <th id="restrictTable">Flashlights<br></br>(Black)</th>
-              <th id="restrictTable">Flashlights<br></br>(Yellow)</th>
-              <th id="restrictTable">Lanterns<br></br>(Yellow)</th>
-              {/* <th>Lanterns<br></br>(Black)</th> */}
-              <th id="restrictTable">Total Owed</th>
-            </tr>
-            </table>
-            {this.state.students.map(student =>(
-              <table class="InProgressTable">
-              <tr id="TableHeader">
-              <th id="restrictTable">{student.first_name} {student.last_name}</th>
-              <th id="restrictTable"></th>
-              <th id="restrictTable"></th>
-              <th id="restrictTable"></th>
-              <th id="restrictTable"></th>
-              <th id="restrictTable"></th>
-              <th id="restrictTable"></th>
-              <th id="restrictTable"></th>
+          {student.map(student => (
+            <table class="InProgressTable">
+              <tr id="TableHeader2">
+                <th id="restrictTable">{student.first_name}<br></br> {student.last_name}</th>
+                <th id="restrictTable">Customer<br></br></th>
+                <th id="restrictTable">Contact</th>
+                <th id="restrictTable">Date Ordered</th>
+                <th id="restrictTable">Flashlights<br></br>(Black)</th>
+                <th id="restrictTable">Flashlights<br></br>(Yellow)</th>
+                <th id="restrictTable">Lanterns <br></br>(Yellow)</th>
+                <th id="restrictTable">Total Owed</th>
               </tr>
-              {orders.filter((element)=> { return element.user_id === student.user_id}).map(orders=> (
+              {orders.filter((element) => { return element.user_id === student.user_id }).map(orders => (
                 <tr id="DataRow" key={orders.order_id}>
-                   
-                 <td id="restrictTable"></td>
-                 <td id="restrictTable">{orders.first_name_cust}  {orders.last_name_cust}</td>
-                 <td id="restrictTable">{orders.phone_cust}<br></br>{orders.email_cust}</td>
-                 <td id="restrictTable"><Moment format="MM/DD/YYYY">{orders.date}</Moment></td>
-                 <td id="restrictTable">{orders.flashlights}</td>
-                 <td id="restrictTable">{orders.flashlight_yellow}</td>
-                 <td id="restrictTable">{orders.pucs}</td>
-                 {/* <td>{orders.puc_black}</td> */}
-                 <td id="TotalData">{'$' + (orders.flashlights * 30 + orders.flashlight_yellow * 30 + orders.pucs * 35 + orders.puc_black * 35)}   <i class="fas fa-exclamation-triangle"></i></td>
- 
-               </tr>
+                  <td id="restrictTable"></td>
+                  <td id="restrictTable">{orders.first_name_cust}  {orders.last_name_cust}</td>
+                  <td id="restrictTable">{orders.phone_cust}<br></br>{orders.email_cust}</td>
+                  <td id="restrictTable"><Moment format="MM/DD/YYYY">{orders.date}</Moment></td>
+                  <td id="restrictTable">{orders.flashlights}</td>
+                  <td id="restrictTable">{orders.flashlight_yellow}</td>
+                  <td id="restrictTable">{orders.pucs}</td>
+                  {/* <td>{orders.puc_black}</td> */}
+                  <td id="TotalData">{'$' + (orders.flashlights * 30 + orders.flashlight_yellow * 30 + orders.pucs * 35 + orders.puc_black * 35)}   <i class="fas fa-exclamation-triangle"></i></td>
+                </tr>
               ))}
-              </table>
-            ))}
+              {function calculate() {
+                let filtered = orders.filter((element) => { return element.user_id === student.user_id })
+                const totalFlash = 0
+                for (let i = 0; i < filtered.length; i++) {
+                  totalFlash += filtered[i].flashlights
+                }
+              }
+              }
+              <tr id="DataRowTotal">
+                <td id="restrictTable">TOTALS</td>
+                <td id="restrictTable"></td>
+                <td id="restrictTable"></td>
+                <td id="restrictTable"></td>
+                <td id="restrictTable">{this.calculate(orders, student).totalFlashBlack}</td>
+                <td id="restrictTable">{this.calculate(orders, student).totalFlashYellow}</td>
+                <td id="restrictTable">{this.calculate(orders, student).totalLanternYellow}</td>
+                <td id="restrictTable">${this.calculate(orders, student).totalPrice.toFixed(2)}</td>
+              </tr>
+            </table>
+          ))}
 
-    
-           
-          
+
+
+
         </div>
 
 
