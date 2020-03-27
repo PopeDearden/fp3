febe467a7640c4147445f591a7efdfeb7dd3f934
taylor dearden <taylordearden@gmail.com>
2020-03-27 14:18:25 -0600

    edited director dash

--- src/components/Director/DirectorDash.jsx
+++ src/components/Director/DirectorDash.jsx
@@ -9,19 +9,42 @@ class DirectorDash extends Component {
     super()
     this.state = {
       remaining: {},
-
+      director: {},
     }
   }
-  componentDidMount () {
+  componentDidMount() {
     this.getRemaining()
+    this.getSamples()
   }
-getRemaining = () => {
-  axios.get('/api/director/remaining').then(res => {
-    this.setState({
-      remaining: res.data[0]
+  getRemaining = async () => {
+    await axios.get('/api/director/remaining').then(res => {
+      this.setState({
+        remaining: res.data[0]
+      })
     })
-  })
-}
+  }
+  getSamples = async () => {
+    await axios.get('/api/director/samples').then(res => {
+      this.setState({
+        director: res.data.samples[0]
+      })
+    })
+  }
+
+  tOrF = (check) => {
+    if (check === true) {
+      return (
+        <i id="good" class="fas fa-check"></i>
+        // <div>...in progress</div>
+      //  <div></div>
+        )
+      }
+      else {
+        return (
+        <div></div>
+      )
+    }
+  }
 
   render() {
     return (
@@ -30,18 +53,31 @@ getRemaining = () => {
           <div className="Title-Bar">
             <h2>Director Dashboard</h2>
           </div>
+          <div class="Split">
+
             <div className="A-Box">
               <h2>Remaining Samples</h2>
-              <p>
-                Black flashlights: {this.state.remaining.remaining_black_flash}
+              <div className="B-Box">
+                <p>
+                  Black flashlights: <b id="bold">{this.state.remaining.remaining_black_flash}</b>
                 </p>
                 <p>
-                  Yellow flashlights: {this.state.remaining.remaining_yellow_flash}
+                  Yellow flashlights:  <b id="bold">{this.state.remaining.remaining_yellow_flash}</b>
                 </p>
                 <p>
-                  Yellow lanterns: {this.state.remaining.remaining_yellow_puc}
+                  Yellow lanterns:  <b id="bold">{this.state.remaining.remaining_yellow_puc}</b>
                 </p>
+              </div>
             </div>
+            <div className="A-Box2">
+              <h2>Other Info</h2>
+              <div className="B-Box">
+                <p> Sample Order Sent by HybridLight: {this.tOrF(this.state.director.sample_processed)} </p>
+               
+       
+              </div>
+            </div>
+          </div>
 
         </div>
       </div>
