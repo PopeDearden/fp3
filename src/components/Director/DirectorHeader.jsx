7fca9ac26a1aa717e695db2a254ccbb006ed3e3c
taylor dearden <taylordearden@gmail.com>
2020-04-14 12:01:43 -0600

    updated with logo

--- src/components/Director/DirectorHeader.jsx
+++ src/components/Director/DirectorHeader.jsx
@@ -44,7 +44,10 @@ class DirectorHeader extends Component {
 
                 <div class="Header">
                     <div class="Animate-Lines" onClick={() => this.props.history.push('/director/')}>
-                        <h1 id="Fancy-Title-2-white">Fundraiser Portal</h1>
+                        <div class="IconBox">
+                            <img id="Icon" src="https://lh3.googleusercontent.com/xNMBgF0KojU7K9wRVZb4oGCBDjcGCLgT8pzCYsp_CWTIMQpW8-1zH0aMrnB2ul2mplP7ZCKQs6eVKa4lkpbfF6_c9osB41LLvBwxLcC5joT-WIqwyMpEhYbabBTRo0CS9XB6alxRYLefdKkP_Owyula2_zPpUwFtSahi82d_VLx57xrHvgwq7ze62NuoA4P_YB6AfBl7gkPEam13IAYxgeeivp1uUCIwAm3ExllHuzu1feXOQTKnJ-GfdgVLTnQytSDoIwAibEjOJElGSenXE8mcKt1drnidn5WjJIPR4fPJ-or63pqdClutfWkNNb7oS12OJBh_9vqW9DHGpFQ2p6xS0YJHAX6aIzSWYvUW38e0GelmB73RG_fnyZgReC8oZoDtR_t9zwlwY9ltQuW1BoDFVGlm1HdGrqV-9XEjAFExOOKUaWbmPbjssg6LiJBJShdkw63FocH7PD2IUoAfX_klMWuYBxdVDJ0IsJT3T-vg8rV11yg4xuK_xY9Tj6qRQkrxFJjhXMSD2j6gNV4mpFuhm-37dzGJaYgNZeTYnColxzthIc2aTVbN1Eo9gB-KKOx9L_kJSSA-nCD0VYxQ-9npb-Qzqvst8aOAXBH81jPnsG_pz0jn1qfqDRJ9wk9zXXGcTWvXVx3Ku3iidH22JTxhnOsgvX2tuzjVsxglBf4711svTWU_5DBsloyXOrAXxNG8ag5EFTetyGsfuxSD58b9a_Sb0sqAM05rBNS6q9o_zmDSW1WY4L4=s756-no" alt="Icon" />
+                            {/* <h1 id="Fancy-Title-2-white">Fundraiser Portal</h1> */}
+                        </div>
                         <div class="Animate-Lines__horizontal"></div>
                         <div class="Animate-Lines__vertical"></div>
                     </div>
@@ -90,11 +93,14 @@ class DirectorHeader extends Component {
             return (
                 <div class="Header">
                     <div class="Animate-Lines" onClick={() => this.props.history.push('/director/')}>
-                        <h1 id="Fancy-Title-2-white">Fundraiser Portal</h1>
+                        <div class="IconBox">
+                            <img id="Icon" src="https://lh3.googleusercontent.com/xNMBgF0KojU7K9wRVZb4oGCBDjcGCLgT8pzCYsp_CWTIMQpW8-1zH0aMrnB2ul2mplP7ZCKQs6eVKa4lkpbfF6_c9osB41LLvBwxLcC5joT-WIqwyMpEhYbabBTRo0CS9XB6alxRYLefdKkP_Owyula2_zPpUwFtSahi82d_VLx57xrHvgwq7ze62NuoA4P_YB6AfBl7gkPEam13IAYxgeeivp1uUCIwAm3ExllHuzu1feXOQTKnJ-GfdgVLTnQytSDoIwAibEjOJElGSenXE8mcKt1drnidn5WjJIPR4fPJ-or63pqdClutfWkNNb7oS12OJBh_9vqW9DHGpFQ2p6xS0YJHAX6aIzSWYvUW38e0GelmB73RG_fnyZgReC8oZoDtR_t9zwlwY9ltQuW1BoDFVGlm1HdGrqV-9XEjAFExOOKUaWbmPbjssg6LiJBJShdkw63FocH7PD2IUoAfX_klMWuYBxdVDJ0IsJT3T-vg8rV11yg4xuK_xY9Tj6qRQkrxFJjhXMSD2j6gNV4mpFuhm-37dzGJaYgNZeTYnColxzthIc2aTVbN1Eo9gB-KKOx9L_kJSSA-nCD0VYxQ-9npb-Qzqvst8aOAXBH81jPnsG_pz0jn1qfqDRJ9wk9zXXGcTWvXVx3Ku3iidH22JTxhnOsgvX2tuzjVsxglBf4711svTWU_5DBsloyXOrAXxNG8ag5EFTetyGsfuxSD58b9a_Sb0sqAM05rBNS6q9o_zmDSW1WY4L4=s756-no" alt="Icon" />
+                            {/* <h1 id="Fancy-Title-2-white">Fundraiser Portal</h1> */}
+                        </div>
                         <div class="Animate-Lines__horizontal"></div>
                         <div class="Animate-Lines__vertical"></div>
                     </div>
-            
+
                     {/* <div class="New-User" onClick={()=>this.props.history.push('/director/create-user')}>
        <i class="fas fa-user-plus"></i>
        <p>Create Student<br></br> Account</p>
@@ -104,9 +110,9 @@ class DirectorHeader extends Component {
                         <p>Final Order Info</p>
                         <i class="fas fa-user-edit"></i>
                     </div>
-             
-             
-                 
+
+
+
                     <div class="Header-Link" onClick={() => this.props.history.push('/director/user-info-listV2')}>
                         <p>All student<br></br>account info</p>
                         <i class="fas fa-users"></i>
--- src/components/Hybrid/HybridHeader.jsx
+++ src/components/Hybrid/HybridHeader.jsx
@@ -36,7 +36,10 @@ class HybridHeader extends Component {
 
             <div class="Header">
                 <div class="Animate-Lines" onClick={() => this.props.history.push('/hybridlight')}>
-                    <h1 id="Fancy-Title-2-white">Fundraiser Portal</h1>
+                <div class="IconBox">
+                    <img id="Icon" src="https://lh3.googleusercontent.com/xNMBgF0KojU7K9wRVZb4oGCBDjcGCLgT8pzCYsp_CWTIMQpW8-1zH0aMrnB2ul2mplP7ZCKQs6eVKa4lkpbfF6_c9osB41LLvBwxLcC5joT-WIqwyMpEhYbabBTRo0CS9XB6alxRYLefdKkP_Owyula2_zPpUwFtSahi82d_VLx57xrHvgwq7ze62NuoA4P_YB6AfBl7gkPEam13IAYxgeeivp1uUCIwAm3ExllHuzu1feXOQTKnJ-GfdgVLTnQytSDoIwAibEjOJElGSenXE8mcKt1drnidn5WjJIPR4fPJ-or63pqdClutfWkNNb7oS12OJBh_9vqW9DHGpFQ2p6xS0YJHAX6aIzSWYvUW38e0GelmB73RG_fnyZgReC8oZoDtR_t9zwlwY9ltQuW1BoDFVGlm1HdGrqV-9XEjAFExOOKUaWbmPbjssg6LiJBJShdkw63FocH7PD2IUoAfX_klMWuYBxdVDJ0IsJT3T-vg8rV11yg4xuK_xY9Tj6qRQkrxFJjhXMSD2j6gNV4mpFuhm-37dzGJaYgNZeTYnColxzthIc2aTVbN1Eo9gB-KKOx9L_kJSSA-nCD0VYxQ-9npb-Qzqvst8aOAXBH81jPnsG_pz0jn1qfqDRJ9wk9zXXGcTWvXVx3Ku3iidH22JTxhnOsgvX2tuzjVsxglBf4711svTWU_5DBsloyXOrAXxNG8ag5EFTetyGsfuxSD58b9a_Sb0sqAM05rBNS6q9o_zmDSW1WY4L4=s756-no" alt="Icon" />
+                    {/* <h1 id="Fancy-Title-2-white">Fundraiser Portal</h1> */}
+                    </div>
                     <div class="Animate-Lines__horizontal"></div>
                     <div class="Animate-Lines__vertical"></div>
                 </div>
