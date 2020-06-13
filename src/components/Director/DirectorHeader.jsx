a521df5c543b6eb7035bac05f9c780ab1ee33735
taylor dearden <taylordearden@gmail.com>
2020-06-13 11:02:25 -0600

    director drop down menu for mobile done

--- src/components/Director/DirectorHeader.jsx
+++ src/components/Director/DirectorHeader.jsx
@@ -51,8 +51,8 @@ class DirectorHeader extends Component {
                 <div class="Header">
                     <div class="Header-Menu">
                         <div class="Split2">
-                        <img id="Icon3" src="https://lh3.googleusercontent.com/FSJw-4kp3OzpqqfvlCwHai1eO604ZVDMP5nl2BsY3B_lX7WOGQItJQGnyliCFcXBfKZIpfhNh2aUYjLhqv39AVczFVOP2FedJf50WmWchF0ofKsHVhmhcFsA1JUx5vGICMnftmv4iMlBww88qZOqPQN0Ol-I5a1rZm4Uke8QVRqAv1VimOXzGGnT1hLMqfk5ertOe5kl-1gLiIYnhe8oXIk9iuc5GIIiviyAhIYcolapPcRH0V2zvH5ZW6EEvkiTQi7oMDP3MTXf4ZJiiTqsV3mbgY5GeUql1wHrr4La_M034iDD5Cj0deH7dwegK77t6Xu_jgDhDFkeCE83J6gapHpkYPppIETCputEV5zKCKBpKmxmCSaFE4-tSsffLbU7pqXiFhB_HoiMT5RGeyQlzWACQ_8fylVmFA7R-zTKH1S5-wHRIxBDG6_Q4yrJhwf8DzH0s111jAr5BK42LV-uJwwmirzOCOquMSj-8gEQMmtNtHJqIDoBozIQLratMoDm6Fd4EfhZ2oCIGGkLly_uuHt6_0jfFG0E280QzEFDp6ZF0BBftME3Y9YdVbr-zdbiND3ZmFUc4ZBGlrxRkxgQYmZYan7UdjEJK_8fk_x3JxlaXFi8YI5xQ7ZNHz8COPEcbHO4UR2ZPS4D52bYeWZhATigukM8V_7oRkVlfxR-eFwwic4Mo5w2JD2WT3kVvlgOl8H8ZjnrA0Oxt7TVCMBrOUWV7hq0qOejjJjuoaKcQxw8igNYk7_A7k0=s756-no" alt="Icon" />
-                        <h1>Fundraiser Portal</h1>
+                            <img id="Icon3" src="https://lh3.googleusercontent.com/FSJw-4kp3OzpqqfvlCwHai1eO604ZVDMP5nl2BsY3B_lX7WOGQItJQGnyliCFcXBfKZIpfhNh2aUYjLhqv39AVczFVOP2FedJf50WmWchF0ofKsHVhmhcFsA1JUx5vGICMnftmv4iMlBww88qZOqPQN0Ol-I5a1rZm4Uke8QVRqAv1VimOXzGGnT1hLMqfk5ertOe5kl-1gLiIYnhe8oXIk9iuc5GIIiviyAhIYcolapPcRH0V2zvH5ZW6EEvkiTQi7oMDP3MTXf4ZJiiTqsV3mbgY5GeUql1wHrr4La_M034iDD5Cj0deH7dwegK77t6Xu_jgDhDFkeCE83J6gapHpkYPppIETCputEV5zKCKBpKmxmCSaFE4-tSsffLbU7pqXiFhB_HoiMT5RGeyQlzWACQ_8fylVmFA7R-zTKH1S5-wHRIxBDG6_Q4yrJhwf8DzH0s111jAr5BK42LV-uJwwmirzOCOquMSj-8gEQMmtNtHJqIDoBozIQLratMoDm6Fd4EfhZ2oCIGGkLly_uuHt6_0jfFG0E280QzEFDp6ZF0BBftME3Y9YdVbr-zdbiND3ZmFUc4ZBGlrxRkxgQYmZYan7UdjEJK_8fk_x3JxlaXFi8YI5xQ7ZNHz8COPEcbHO4UR2ZPS4D52bYeWZhATigukM8V_7oRkVlfxR-eFwwic4Mo5w2JD2WT3kVvlgOl8H8ZjnrA0Oxt7TVCMBrOUWV7hq0qOejjJjuoaKcQxw8igNYk7_A7k0=s756-no" alt="Icon" />
+                            <h1>Fundraiser Portal</h1>
                         </div>
                         <i onClick={() => this.setState(prevState => ({
                             menu: !prevState.menu
@@ -111,7 +111,15 @@ class DirectorHeader extends Component {
         }
         if (this.state.stage === "order sent") {
             return (
-                <div class="Header">
+                <div class="Header">\<div class="Header-Menu">
+                    <div class="Split2">
+                        <img id="Icon3" src="https://lh3.googleusercontent.com/FSJw-4kp3OzpqqfvlCwHai1eO604ZVDMP5nl2BsY3B_lX7WOGQItJQGnyliCFcXBfKZIpfhNh2aUYjLhqv39AVczFVOP2FedJf50WmWchF0ofKsHVhmhcFsA1JUx5vGICMnftmv4iMlBww88qZOqPQN0Ol-I5a1rZm4Uke8QVRqAv1VimOXzGGnT1hLMqfk5ertOe5kl-1gLiIYnhe8oXIk9iuc5GIIiviyAhIYcolapPcRH0V2zvH5ZW6EEvkiTQi7oMDP3MTXf4ZJiiTqsV3mbgY5GeUql1wHrr4La_M034iDD5Cj0deH7dwegK77t6Xu_jgDhDFkeCE83J6gapHpkYPppIETCputEV5zKCKBpKmxmCSaFE4-tSsffLbU7pqXiFhB_HoiMT5RGeyQlzWACQ_8fylVmFA7R-zTKH1S5-wHRIxBDG6_Q4yrJhwf8DzH0s111jAr5BK42LV-uJwwmirzOCOquMSj-8gEQMmtNtHJqIDoBozIQLratMoDm6Fd4EfhZ2oCIGGkLly_uuHt6_0jfFG0E280QzEFDp6ZF0BBftME3Y9YdVbr-zdbiND3ZmFUc4ZBGlrxRkxgQYmZYan7UdjEJK_8fk_x3JxlaXFi8YI5xQ7ZNHz8COPEcbHO4UR2ZPS4D52bYeWZhATigukM8V_7oRkVlfxR-eFwwic4Mo5w2JD2WT3kVvlgOl8H8ZjnrA0Oxt7TVCMBrOUWV7hq0qOejjJjuoaKcQxw8igNYk7_A7k0=s756-no" alt="Icon" />
+                        <h1>Fundraiser Portal</h1>
+                    </div>
+                    <i onClick={() => this.setState(prevState => ({
+                        menu: !prevState.menu
+                    }))} class="fas fa-bars"></i>
+                </div>
                     <div class="Animate-Lines" onClick={() => this.props.history.push('/director/')}>
                         <div class="IconBox">
                             <img id="Icon" src="https://lh3.googleusercontent.com/FSJw-4kp3OzpqqfvlCwHai1eO604ZVDMP5nl2BsY3B_lX7WOGQItJQGnyliCFcXBfKZIpfhNh2aUYjLhqv39AVczFVOP2FedJf50WmWchF0ofKsHVhmhcFsA1JUx5vGICMnftmv4iMlBww88qZOqPQN0Ol-I5a1rZm4Uke8QVRqAv1VimOXzGGnT1hLMqfk5ertOe5kl-1gLiIYnhe8oXIk9iuc5GIIiviyAhIYcolapPcRH0V2zvH5ZW6EEvkiTQi7oMDP3MTXf4ZJiiTqsV3mbgY5GeUql1wHrr4La_M034iDD5Cj0deH7dwegK77t6Xu_jgDhDFkeCE83J6gapHpkYPppIETCputEV5zKCKBpKmxmCSaFE4-tSsffLbU7pqXiFhB_HoiMT5RGeyQlzWACQ_8fylVmFA7R-zTKH1S5-wHRIxBDG6_Q4yrJhwf8DzH0s111jAr5BK42LV-uJwwmirzOCOquMSj-8gEQMmtNtHJqIDoBozIQLratMoDm6Fd4EfhZ2oCIGGkLly_uuHt6_0jfFG0E280QzEFDp6ZF0BBftME3Y9YdVbr-zdbiND3ZmFUc4ZBGlrxRkxgQYmZYan7UdjEJK_8fk_x3JxlaXFi8YI5xQ7ZNHz8COPEcbHO4UR2ZPS4D52bYeWZhATigukM8V_7oRkVlfxR-eFwwic4Mo5w2JD2WT3kVvlgOl8H8ZjnrA0Oxt7TVCMBrOUWV7hq0qOejjJjuoaKcQxw8igNYk7_A7k0=s756-no" alt="Icon" />
@@ -125,26 +133,28 @@ class DirectorHeader extends Component {
        <i class="fas fa-user-plus"></i>
        <p>Create Student<br></br> Account</p>
    </div> */}
+                    <div class={`Header-List-${this.state.menu}`}>
+                        <div class="Header-Link" onClick={() => this.props.history.push('/print/director-placed-print')}>
+                            <p>All Student Orders</p>
+                            <i class="fas fa-user-edit"></i>
+                        </div>
+                        <div class="Header-Link" onClick={() => this.props.history.push('/print/director-balances')}>
+                            <p>Student Balances</p>
+                            <i class="fas fa-balance-scale"></i>
+                        </div>
+                        <div class="Header-Link" onClick={() => this.props.history.push('/director/user-info-listV2')}>
+                            <p>All student<br></br>account info</p>
+                            <i class="fas fa-users"></i>
+                        </div>
+                        <div class="Header-Link" onClick={() => this.props.history.push('/director/invoice')}>
+                            <p>Invoice</p>
+                            <i class="fas fa-file-invoice-dollar"></i>
+                        </div>
+                        <div class="Header-Link" onClick={() => this.logout()}>
+                            <p>Logout</p>
+                            <i class="fas fa-sign-out-alt"></i>
+                        </div>
 
-                    <div class="Header-Link" onClick={() => this.props.history.push('/print/director-placed-print')}>
-                        <p>All Student Orders</p>
-                        <i class="fas fa-user-edit"></i>
-                    </div>
-                    <div class="Header-Link" onClick={() => this.props.history.push('/print/director-balances')}>
-                        <p>Student Balances</p>
-                        <i class="fas fa-balance-scale"></i>
-                    </div>
-                    <div class="Header-Link" onClick={() => this.props.history.push('/director/user-info-listV2')}>
-                        <p>All student<br></br>account info</p>
-                        <i class="fas fa-users"></i>
-                    </div>
-                    <div class="Header-Link" onClick={() => this.props.history.push('/director/invoice')}>
-                        <p>Invoice</p>
-                        <i class="fas fa-file-invoice-dollar"></i>
-                    </div>
-                    <div class="Header-Link" onClick={() => this.logout()}>
-                        <p>Logout</p>
-                        <i class="fas fa-sign-out-alt"></i>
                     </div>
                 </div>
             )
